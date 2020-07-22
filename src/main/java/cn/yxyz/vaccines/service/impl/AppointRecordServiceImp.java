4d4e15848348a3f5a2d4e273d15365eebc7e9d4c
yangchenghu <670989882@qq.com>
2020-07-22 21:44:39 +0800

    feat:添加体检预约逻辑

--- src/main/java/cn/yxyz/vaccines/service/impl/AppointRecordServiceImp.java
+++ src/main/java/cn/yxyz/vaccines/service/impl/AppointRecordServiceImp.java
@@ -4,10 +4,12 @@ import cn.yxyz.vaccines.mapper.AppointRecordMapper;
 
 import cn.yxyz.vaccines.pojo.AppointRecord;
 
+import cn.yxyz.vaccines.pojo.User;
 import cn.yxyz.vaccines.service.AppointRecordService;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -34,5 +36,14 @@ public class AppointRecordServiceImp implements AppointRecordService {
         QueryWrapper<AppointRecord> wrapper=new QueryWrapper<>();
         wrapper.eq("numberid",numberid);
         return appointrecordMapper.selectList(wrapper);
+    }
+
+    @Transactional
+    public synchronized String appoint(/*参数*/) {
+        //取出项目余额
+        //项目余额小于等于0 return "预约失败，该日期人数已满";
+        //项目余额减一
+        //增加预约表
+        return "预约成功";
     }
 }
